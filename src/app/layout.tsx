85cd1ee5f916fa64a6a889c2b75a292b3d5eda49
Eduardo Reis <edu_drozda@hotmail.com>
2023-11-28 14:06:00 -0300

    force deplloy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,7 @@ const poppins = Poppins({
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: {children: React.ReactNode;}) {
   return (
     <html lang="pt-br'">
       <body className={`${poppins.className}`}>{children}</body>
